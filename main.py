29cb96966cb52a004d5f3113d8b8ad5e57a1cc71
Joaquin Pujols <54540355+jpujols@users.noreply.github.com>
2022-04-03 15:32:46 +0000

    Define libraries and download perception English words. This will serve as a db to search our questions to the chatbox

---
+++ main.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+nltk.download('averaged_perception_tagger')
+nltk.download('wordnet')
+nltk.download('punkt')
+
+text = 'Originally, vegetables were collected from the wild by hunters-gatheres. Vegetables are all plants. Vegetables can be eaten either raw or cooked.'
+question = 'What are plants?'
+
+lemmatizer = WordNetLemmatizer()
+
+def lemma_me(sent):
+  sentence_tokens = nltkl.word_tokenize(sent.lower())
+  pos_tags = nltk.pos_tag(sentence_tokens)
+
+  sentence_lemmas = []
+  for token, pos_tag in zip(sentence_tokens, pos_tags):
+    if pos_tags[1][0].lower() in ['n', 'v', 'a', 'r']:
+      lemma = lemmatizer.lemmatize(token, pos_tag[1[0]].lower())
+      return sentence_lemmas
+
+sentence_tokens = nltk.sent_tokenize(text)
+sentence_tokens.append(question)
+
